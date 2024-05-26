17d058725023e8dc283a9b8ea6245ca45bf7897a
Simeon-2022 <simeonalexandrov931@gmail.com>
2024-05-26 23:15:03 +0300

    StacksAndQueues

--- _03_Java_Advanced_May_2024/src/_03_Stacks_And_Queues_Lab/_07_Math_Potato.java
+++ _03_Java_Advanced_May_2024/src/_03_Stacks_And_Queues_Lab/_07_Math_Potato.java
@@ -1,8 +1,55 @@
 package _03_Stacks_And_Queues_Lab;
 
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+
 public class _07_Math_Potato {
     public static void main(String[] args) {
 
-        
+        Scanner scanner = new Scanner(System.in);
+
+        String[] arr = scanner.nextLine().split(" ");
+        int round = scanner.nextInt();
+         PriorityQueue<String> names = new PriorityQueue<>();
+        Collections.addAll(names, arr);
+
+        int rounds = 0;
+        while (names.size() > 1) {
+            rounds++;
+       /*     for (int i = 1; i <= round; i++) {
+
+                names.offer(names.poll());
+            }*/
+            if (isPrime(rounds)) {
+                System.out.println("Prime " + names.peek());
+            } else {
+                System.out.println("Removed " + names.poll());
+            }
+        }
+        System.out.println("Last is " + names.peek());
+    }
+
+    public static boolean isPrime(int number) {
+        boolean isNumberPrime = true;
+
+        int counter = 0;
+
+        if (number == 1) {
+            return false;
+        }
+
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                if (++counter > 2) {
+                    isNumberPrime = false;
+                    break;
+                }
+            }
+        }
+        return isNumberPrime;
     }
 }
+
---
+++ _03_Java_Advanced_May_2024/src/_03_Stacks_And_Queues_Lab/_08_Browser_History_Upgrade.java
@@ -0,0 +1,4 @@
+package _03_Stacks_And_Queues_Lab;
+
+public class _08_Browser_History_Upgrade {
+}
