a7ba585de18bf2497f7b4f78b47a7d202065015a
Simeon-2022 <simeonalexandrov931@gmail.com>
2024-05-26 21:16:07 +0300

    StacksAndQueues

--- _03_Java_Advanced_May_2024/src/_03_Stacks_And_Queues_Lab/_06_Hot_Potato.java
+++ _03_Java_Advanced_May_2024/src/_03_Stacks_And_Queues_Lab/_06_Hot_Potato.java
@@ -1,4 +1,25 @@
 package _03_Stacks_And_Queues_Lab;
 
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class _06_Hot_Potato {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] arr = scanner.nextLine().split(" ");
+        int round = scanner.nextInt();
+        ArrayDeque<String> names = new ArrayDeque<>();
+        Collections.addAll(names, arr);
+
+        while (names.size() > 1) {
+
+            for (int i = 0; i < round; i++) {
+                names.offerLast(names.removeFirst());
+            }
+            System.out.println("Removed " + names.removeLast());
+        }
+        System.out.println("Last is " + names.peek());
+    }
 }
---
+++ _03_Java_Advanced_May_2024/src/_03_Stacks_And_Queues_Lab/_07_Math_Potato.java
@@ -0,0 +1,8 @@
+package _03_Stacks_And_Queues_Lab;
+
+public class _07_Math_Potato {
+    public static void main(String[] args) {
+
+        
+    }
+}
