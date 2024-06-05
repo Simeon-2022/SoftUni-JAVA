dbc30aac53126ecee005c25b2659b519f2fcea52
Simeon-2022 <simeonalexandrov931@gmail.com>
2024-06-05 22:12:33 +0300

    _05_MultidimentionalArrays

--- _03_Java_Advanced_May_2024/src/_05_Multidimentional_Arrays_Lab/_01_Compare_Matrices.java
+++ _03_Java_Advanced_May_2024/src/_05_Multidimentional_Arrays_Lab/_01_Compare_Matrices.java
@@ -1,7 +1,61 @@
 package _05_Multidimentional_Arrays_Lab;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class _01_Compare_Matrices {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        int[] matrxParams = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int[][] matrx1 = new int[matrxParams[0]][matrxParams[1]];
+
+        fillMatrix(matrx1,scanner);
+
+        matrxParams = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int[][] matrx2 = new int[matrxParams[0]][matrxParams[1]];
+
+        fillMatrix(matrx2,scanner);
+
+        if (matrxCompare(matrx1, matrx2)) {
+            System.out.println("equal");
+        } else {
+            System.out.println("not equal");
+
+        }
+    }
+
+    public static void fillMatrix(int[][] matrx, Scanner scanner) {
+
+        for (int i = 0; i < matrx.length; i++) {
+            matrx[i] = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+    }
+
+    public static boolean matrxCompare(int[][] matrx1,int[][] matrx2 ) {
+
+        if (matrx1.length != matrx2.length) {
+            return false;
+
+        } else {
+            for (int row = 0; row < matrx1.length; row++) {
+
+                if (matrx1[row].length != matrx2[row].length) {
+                    return false;
+
+                } else {
+                    for (int col = 0; col < matrx1[row].length; col++) {
+
+                        if (matrx1[row][col] != matrx1[row][col]) {
+                        return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
     }
 }
---
+++ _03_Java_Advanced_May_2024/src/_05_Multidimentional_Arrays_Lab/_02_Positions_Of.java
@@ -0,0 +1,7 @@
+package _05_Multidimentional_Arrays_Lab;
+
+public class _02_Positions_Of {
+    public static void main(String[] args) {
+        
+    }
+}
