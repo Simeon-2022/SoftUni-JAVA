ee2a344d3416a7781f0beb58ca907c932dec1d53
Simeon-2022 <simeonalexandrov931@gmail.com>
2024-06-17 15:45:47 +0300

    Set and Maps Advanced

--- _03_Java_Advanced_May_2024/src/_07_Sets_And_Maps_Advanced_Lab/_02_SoftUni_Party.java
+++ _03_Java_Advanced_May_2024/src/_07_Sets_And_Maps_Advanced_Lab/_02_SoftUni_Party.java
@@ -1,7 +1,53 @@
 package _07_Sets_And_Maps_Advanced_Lab;
 
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.TreeSet;
+
 public class _02_SoftUni_Party {
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+
+        TreeSet<String> vip = new TreeSet<>();
+        TreeSet<String> regular = new TreeSet<>();
+
+        String input = scanner.nextLine();
+        while (!"PARTY".equalsIgnoreCase(input)) {
+
+            if (Character.isDigit(input.charAt(0))) {
+
+                vip.add(input);
+            } else {
+
+                regular.add(input);
+            }
+
+            input = scanner.nextLine();
+        }
+
+        while (!"END".equalsIgnoreCase(input)) {
+
+            if (Character.isDigit(input.charAt(0))) {
+
+                vip.remove(input);
+            } else {
+
+                regular.remove(input);
+            }
+
+            input = scanner.nextLine();
+        }
+
+        System.out.println(vip.size() + regular.size());
+
+        for (String v : vip) {
+            System.out.println(v);
+        }
+
+        for (String r : regular) {
+            System.out.println(r);
+        }
+
     }
 }
