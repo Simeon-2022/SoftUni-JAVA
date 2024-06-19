f6c1431e98cdc59959c4252912f49b73568a7dbb
Simeon-2022 <simeonalexandrov931@gmail.com>
2024-06-19 15:06:08 +0300

    Set and Maps Advanced

--- _03_Java_Advanced_May_2024/src/_08_Sets_And_Maps_Advanced_Exercise/_03_Periodic_Table.java
+++ _03_Java_Advanced_May_2024/src/_08_Sets_And_Maps_Advanced_Exercise/_03_Periodic_Table.java
@@ -1,9 +1,6 @@
 package _08_Sets_And_Maps_Advanced_Exercise;
 
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.Scanner;
-import java.util.TreeSet;
+import java.util.*;
 
 public class _03_Periodic_Table {
     public static void main(String[] args) {
@@ -11,20 +8,22 @@ public class _03_Periodic_Table {
 
         TreeSet<String> periodicTable = new TreeSet<>();
 
-        for (int i = 0; i < 4; i++) {
-            periodicTable.add(scanner.nextLine());
+        int rounds = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < rounds; i++) {
+            periodicTable.addAll(Arrays.stream(scanner.nextLine().split(" ")).toList());
         }
 
-        Iterator<String> iter = periodicTable.descendingIterator();
-            while (iter.hasNext()) {
-                System.out.println(iter.next());
-            }
+        //Iterator<String> iter = periodicTable.descendingIterator();
+        Iterator<String> iter = periodicTable.iterator();
+
+        while (iter.hasNext()) {
+            System.out.print(iter.next() + " ");
+        }
 
 
-       // LinkedHashSet<String> reversed = new LinkedHashSet<>();
-       // reversed.addAll(periodicTable.descendingSet());
-       //LinkedHashSet<String> reversed = new LinkedHashSet<>(periodicTable.descendingSet());
-
+        // LinkedHashSet<String> reversed = new LinkedHashSet<>();
+        // reversed.addAll(periodicTable.descendingSet());
+        //LinkedHashSet<String> reversed = new LinkedHashSet<>(periodicTable.descendingSet());
 
 
     }
