bfd1a6af0019b199947250b3ce4371b1c62429dd
Simeon-2022 <simeonalexandrov931@gmail.com>
2024-06-29 19:20:26 +0300

    OOP

--- _04_Java_OOP_June_2024/src/_03_Working_with_Abstarction_Lab/_01_Rhombus_of_Stars/_01_Rhombus_of_Stars.java
+++ _04_Java_OOP_June_2024/src/_03_Working_with_Abstarction_Lab/_01_Rhombus_of_Stars/_01_Rhombus_of_Stars.java
@@ -1,4 +1,51 @@
 package _03_Working_with_Abstarction_Lab._01_Rhombus_of_Stars;
 
+import java.util.Scanner;
+
 public class _01_Rhombus_of_Stars {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int tokenCount = Integer.parseInt(scanner.nextLine());
+
+        printTopPart("* ", tokenCount);
+        printMiddlePart("* ", tokenCount);
+        printBottomPart("* ", tokenCount);
+    }
+
+    public static void printToken(String token, int count) {
+        int start = 0;
+        while (start < count) {
+            System.out.print(token);
+            start++;
+        }
+    }
+
+    public static void printTopPart(String token, int starCount) {
+
+        for (int row = 1; row < starCount; row++) {
+
+            printToken(" ", starCount - row);
+
+            printToken(token, row);
+
+            System.out.println();
+        }
+    }
+
+    public static void printMiddlePart(String token, int starCount) {
+        printToken(token, starCount);
+        System.out.println();
+    }
+
+    public static void printBottomPart(String token, int starCount) {
+        for (int row = 1;  row < starCount; row++) {
+
+            printToken(" ",row);
+
+            printToken(token, starCount - row);
+
+            System.out.println();
+        }
+    }
 }
